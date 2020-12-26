56ac1af9afd7ac5e9d645544443a16af38a62f73
Erik Wegner <E_Wegner@web.de>
2020-12-26 22:39:32 +0100

    twig

--- src/Controller/FrontendModule/SgrfEditFormController.php
+++ src/Controller/FrontendModule/SgrfEditFormController.php
@@ -8,6 +8,7 @@ use Contao\ModuleModel;
 use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 /**
  * @FrontendModule("sgrf_antrag",
@@ -15,10 +16,32 @@ use Symfony\Component\HttpFoundation\Response;
  *   template="mod_sgrf_edit_form"
  * )
  */
-class SgrfEditFormController extends AbstractFrontendModuleController
-{
-    protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
-    {
-        return $template->getResponse();
+class SgrfEditFormController extends AbstractFrontendModuleController {
+
+    private $twig;
+
+    public function __construct(Environment $twig) {
+        $this->twig = $twig;
     }
+
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response {
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        $template->text = 'Hallo welt 2';
+        $template->form = $form->createView();
+
+        return new Response($this->twig->render(
+            'sgrf_edit_form.html.twig',
+            [
+                'form' => $form,
+                'text' => 'Hallo twig welt',
+            ]
+        )); 
+    }
+
 }
